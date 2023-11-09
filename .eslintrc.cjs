49e8af18786ed5b83d7874fd2ea89e0e26872ce6
icvitkov <ivana.cvit@hotmail.com>
2023-11-09 11:26:17 +0100

    add vite plugin pages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,11 +3,19 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  overrides: [
+    {
+      files: ['src/pages/**/*.vue'],
+      rules: {
+        'vue/multi-word-component-names': 'off' // Disable the rule
+      }
+    }
   ],
   parserOptions: {
     ecmaVersion: 'latest'
